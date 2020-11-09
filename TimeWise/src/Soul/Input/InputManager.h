1df8f6dda26477427426190258f18da2e97cddb4
Bounty556 <jacobmayday@gmail.com>
2020-11-09 10:43:32 -0700

    Added documentation for InputManager

--- TimeWise/src/Soul/Input/InputManager.h
+++ TimeWise/src/Soul/Input/InputManager.h
@@ -18,26 +18,64 @@ namespace Soul
 	the playerNumber whose input you're checking for. The keyboard counts as a separate player
 	from the controllers. Players are numbered automatically by the order in which they are
 	added to the controllers list, starting at 0.
+
+	Ex:
+	InputManager::SetAcceptingNewControllers(true);
+
+	// Game loop
+	while (...)
+	{
+		if (InputManager::GetInputInfo(0, "Jump").State & ControlsMap::Pressed)
+		{
+			SoulLogInfo("Player 1 Jumped!");
+		}
+	}
 	*/
 	class InputManager
 	{
 	public:
 		InputManager() = delete;
 		
+		/*
+		Partitions necessary memory for storing controller information.
+		*/
 		static void Init();
 
+		/*
+		Updates the button states and axis information for all connected controllers.
+		*/
 		static void Update();
 
+		/*
+		Used for determining whether a new controller has connected.
+		*/
 		static void ProcessInput(sf::Event& e);
 
+		/*
+		Frees all necessary memory used for storing controller information.
+		*/
 		static void CleanUp();
 
+		/*
+		Allows the input manager to detect and add newly connected controllers.
+		*/
 		static void SetAcceptingNewControllers(bool value);
 
+		/*
+		Returns an InputInfo struct containing the button information for the desired control
+		for a given player.
+		*/
 		static Controller::InputInfo GetInputInfo(unsigned int player, const char* controlString);
 
 	private:
+		/*
+		Vector storing all of the controller information for the connected controllers.
+		*/
 		static Vector<Controller>* m_Controllers;
+
+		/*
+		Whether the InputManager should detect and accept newly connected controllers.
+		*/
 		static bool m_AcceptingNewControllers;
 	};
 }

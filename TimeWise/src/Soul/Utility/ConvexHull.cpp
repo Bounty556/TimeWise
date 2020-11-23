ce730ad1637bf04e78aaaa419bab6e8a84c299fa
Bounty556 <jacobmayday@gmail.com>
2020-11-23 03:38:38 -0700

    Finished Quickhull algorithm in ConvexHull class

--- TimeWise/src/Soul/Utility/ConvexHull.cpp
+++ TimeWise/src/Soul/Utility/ConvexHull.cpp
@@ -1,5 +1,8 @@
 #include "ConvexHull.h"
 
+#include <Structures/Vector.h>
+#include <Utility/Math.h>
+
 namespace Soul
 {
 	ConvexHull::ConvexHull(const UniquePointer<sf::Vector2f>& vertices, unsigned int vertexCount) :
@@ -10,7 +13,7 @@ namespace Soul
 
 	void ConvexHull::GenerateHull(const UniquePointer<sf::Vector2f>& vertices, unsigned int vertexCount)
 	{
-		UniquePointer<sf::Vector2f> tempVertices(PartitionArray(sf::Vector2f, vertexCount));
+		Vector<sf::Vector2f> tempVertices(vertexCount);
 		unsigned int hullVertices = 0;
 
 		// Find min and max x vertices
@@ -37,9 +40,50 @@ namespace Soul
 			}
 		}
 
+		tempVertices.Push(minX);
+		tempVertices.Push(maxX);
+
+		unsigned int currentVertex = 0;
 		while (true)
 		{
-			// TODO: Grab rest of points
+			sf::Vector2f line(m_Vertices[(currentVertex + 1) % tempVertices.Length()] - m_Vertices[currentVertex]);
+			sf::Vector2f direction = Math::Perpendicular(line);
+
+			bool found = false;
+			float magnitude = 0.0f;
+			sf::Vector2f point;
+
+			for (unsigned int i = 0; i < vertexCount; ++i)
+			{
+				float mag = Math::Dot(vertices[i] - m_Vertices[currentVertex], direction);
+				if (mag > magnitude)
+				{
+					magnitude = mag;
+					point = vertices[i];
+					found = true;
+				}
+			}
+
+			if (found)
+			{
+				tempVertices.Insert(currentVertex + 1, point);
+			}
+			else
+			{
+				++currentVertex;
+			}
+
+			if (currentVertex == tempVertices.Length() - 1)
+			{
+				break;
+			}
+		}
+
+		m_Vertices = PartitionArray(sf::Vector2f, tempVertices.Length());
+
+		for (unsigned int i = 0; i < tempVertices.Length(); ++i)
+		{
+			new (&m_Vertices[i]) sf::Vector2f(tempVertices[i]);
 		}
 	}
 }

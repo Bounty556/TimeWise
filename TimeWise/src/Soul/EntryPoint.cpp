675b9ef5f9ba8a1dbeaf8efac34822a104f526dd
Bounty556 <jacobmayday@gmail.com>
2020-11-26 16:16:54 -0700

    Moving devices

--- TimeWise/src/Soul/EntryPoint.cpp
+++ TimeWise/src/Soul/EntryPoint.cpp
@@ -1,15 +1,13 @@
 #include "Application.h"
 
+// TODO: Change Components to use hashed strings
 // TODO: Add Entity tags?
 // TODO: Add Triggers
 // TODO: Add jumping
-// TODO: Add platform movement
 // TODO: Volatile memory management
 // TODO: Networking
 // TODO: Multi-threading
 // TODO: Add random number gen
-// TODO: We may need to change how the managers work. It might be faster to load all needed
-//       textures at once, then use them as needed, as we do with the FontManager.
 
 int main()
 {
